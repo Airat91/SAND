fdb58c85c6551ace334d5b2cebc82d883220d8ba
Air <Gitfanov.Ayrat@yandex.ru>
2025-10-31 04:14:24 +0500

    modbus added

--- inc/modbus_sand.h
+++ inc/modbus_sand.h
@@ -28,21 +28,174 @@ extern "C" {
 
 #define MODBUS_TASK_PERIOD      10
 #define MODBUS_INIT_TIMEOUT_MS  2000
+#define MODBUS_BUF_MAX_LEN      300
+#define MODBUS_SUPRT_FNCT_NMB   4
 
 //--------Macro--------
 
+//========Check GPIO's definitions========
+#ifndef MDB_ADDR_0_PORT
+    #error "MODBUS: Please define MDB_ADDR_0_PORT"
+#endif
+#ifndef MDB_ADDR_0_PIN
+    #error "MODBUS: Please define MDB_ADDR_0_PIN"
+#endif
+#ifndef MDB_ADDR_1_PORT
+    #error "MODBUS: Please define MDB_ADDR_1_PORT"
+#endif
+#ifndef MDB_ADDR_1_PIN
+    #error "MODBUS: Please define MDB_ADDR_1_PIN"
+#endif
+#ifndef MDB_ADDR_2_PORT
+    #error "MODBUS: Please define MDB_ADDR_2_PORT"
+#endif
+#ifndef MDB_ADDR_2_PIN
+    #error "MODBUS: Please define MDB_ADDR_2_PIN"
+#endif
+#ifndef MDB_ADDR_3_PORT
+    #error "MODBUS: Please define MDB_ADDR_3_PORT"
+#endif
+#ifndef MDB_ADDR_3_PIN
+    #error "MODBUS: Please define MDB_ADDR_3_PIN"
+#endif
+#ifndef MDB_ADDR_4_PORT
+    #error "MODBUS: Please define MDB_ADDR_4_PORT"
+#endif
+#ifndef MDB_ADDR_4_PIN
+    #error "MODBUS: Please define MDB_ADDR_4_PIN"
+#endif
+#ifndef MDB_ADDR_5_PORT
+    #error "MODBUS: Please define MDB_ADDR_5_PORT"
+#endif
+#ifndef MDB_ADDR_5_PIN
+    #error "MODBUS: Please define MDB_ADDR_5_PIN"
+#endif
+#ifndef MDB_ADDR_6_PORT
+    #error "MODBUS: Please define MDB_ADDR_6_PORT"
+#endif
+#ifndef MDB_ADDR_6_PIN
+    #error "MODBUS: Please define MDB_ADDR_6_PIN"
+#endif
+#ifndef MDB_ADDR_7_PORT
+    #error "MODBUS: Please define MDB_ADDR_7_PORT"
+#endif
+#ifndef MDB_ADDR_7_PIN
+    #error "MODBUS: Please define MDB_ADDR_7_PIN"
+#endif
+
 //--------Typedefs-------
+
+typedef enum{
+    MODBUS_ST_DISABLE   = 0,
+    MODBUS_ST_READY     = (1<<0),
+}modbus_state_t;
+
+typedef enum{
+    MODBUS_FNCT_RD_MUL_COIL     = 1,    // Read Coil Status
+    MODBUS_FNCT_RD_MUL_DISCR    = 2,    // Read Discrete Inputs
+    MODBUS_FNCT_RD_MUL_HOLD     = 3,    // Read Holding Registers
+    MODBUS_FNCT_RD_MUL_INPUT    = 4,    // Read Input Registers
+    MODBUS_FNCT_WR_SIN_COIL     = 5,    // Force Single Coil
+    MODBUS_FNCT_WR_SIN_HOLD     = 6,    // Preset Single Register
+    MODBUS_FNCT_RD_EXCP_STAT    = 7,    // Read Exception Status
+    MODBUS_FNCT_DIAGNOSTIC      = 8,    // Diagnostic
+
+    MODBUS_FNCT_RD_EVENT_CNT    = 11,   // Get Com Event Counter
+    MODBUS_FNCT_RD_EVENT_LOG    = 12,   // Get Com Event Log
+
+    MODBUS_FNCT_WR_MUL_COIL     = 15,   // Force Multiple Coils
+    MODBUS_FNCT_WR_MUL_HOLD     = 16,   // Preset Multiple Registers
+
+    MODBUS_FNCT_RD_FILE         = 20,   // Read File Record
+    MODBUS_FNCT_WR_FILE         = 21,   // Write File Record
+    MODBUS_FNCT_WR_MASK_REG     = 22,   // Mask Write Register
+    MODBUS_FNCT_RD_WR_MUL_REGS  = 23,   // Read/Write Multiple registers
+    MODBUS_FNCT_RD_FIFO         = 24,   // Read FIFO Queue
+}modbus_function_t;
+
+typedef enum{
+    MODBUS_ERR_FUNCT            = 1,    // Unsupported function
+    MODBUS_ERR_ADDR             = 2,    // Register address error
+    MODBUS_ERR_VALUE            = 3,
+    MODBUS_ERR_PARITY           = 8,    // Parity error
+}modbus_err_t;
+
+typedef struct{
+    modbus_state_t state;
+    u8 out_buf[MODBUS_BUF_MAX_LEN];
+    u16 out_len;
+}modbus_pcb_t;
+
+typedef struct{
+    u8 slave_addr;  // Slave address [0..255]
+    u8 function;    // ModBUS function code
+    u16 reg_addr;   // ModBUS register start address
+    u16 reg_nmb;    // Number of registers
+    u16 crc;        // ModBUS packet CRC16
+    u16* data;      // Pointer to data in packet
+    u8 data_len;    // Lenght of data
+}modbus_packet_t;
 
 //-------External variables------
 
 extern osThreadId modbus_task_handle;
-extern u32 modbus_led_ok_on_time;
-extern u32 modbus_led_err_on_time;
+extern modbus_pcb_t modbus_pcb;
 
 //-------Function prototypes----------
 
+/**
+ * @brief Main task of ModBUS service
+ * @param argument - a pointer to arguments
+ * @ingroup modbus
+ */
 void modbus_task(void const * argument);
 
+/**
+ * @brief Init ModBUS process control block
+ * @ingroup modbus
+ * @param modbus_pcb - pointer to ModBUS proccess control block
+ * @return  0 - ok,\n
+ *          negative value if error
+ */
+int modbus_init(modbus_pcb_t* modbus_pcb);
+
+/**
+ * @brief Deinit ModBUS process control block
+ * @ingroup modbus
+ * @param modbus_pcb - pointer to ModBUS proccess control block
+ * @return  0 - ok,\n
+ *          negative value if error
+ */
+int modbus_deinit(modbus_pcb_t* modbus_pcb);
+
+/**
+ * @brief Read ModBUs address switches state
+ * @ingroup modbus
+ * @return self modbus address
+ */
+int modbus_read_addr(void);
+
+/**
+ * @brief Recognise packet as ModBUS
+ * @param buf - pointer to packet buffer
+ * @param len - lenght of buffer
+ * @ingroup modbus
+ * @return  0 - packet isn't ModBUS
+ *          1 - packet is ModBUS
+ */
+int modbus_packet_recognise(u8* buf, u16 len);
+
+/**
+ * @brief Handle input ModBUS packet
+ * @param modbus_pcb - pointer to ModBUS proccess control block
+ * @param buf - pointer to packet buffer
+ * @param len - lenght of buffer
+ * @return  0 - packet handled, no need response,\n
+ *          1 - packet handled, send response from modbus_pcb.out_buf
+ *          -1 - ModBUS function unsupported
+ *          -2 - unavailable registers address
+ */
+int modbus_packet_handle(modbus_pcb_t* modbus_pcb, u8* buf, u16 len);
 
 
 #ifdef __cplusplus
--- src/modbus_sand.c
+++ src/modbus_sand.c
@@ -10,14 +10,21 @@
 //-------Global variables------
 
 osThreadId modbus_task_handle = {0};
-u32 modbus_led_ok_on_time = 0;
-u32 modbus_led_err_on_time = 0;
+modbus_pcb_t modbus_pcb = {0};
 
 //-------Static variables------
+
+static const modbus_function_t modbus_suprt_fnct_list[MODBUS_SUPRT_FNCT_NMB] = {
+    MODBUS_FNCT_RD_MUL_HOLD,
+    MODBUS_FNCT_RD_MUL_INPUT,
+    MODBUS_FNCT_WR_MUL_COIL,
+    MODBUS_FNCT_WR_MUL_HOLD,
+};
 
 //-------Static functions declaration-----------
 
 static void modbus_gpio_init(void);
+static void modbus_gpio_deinit(void);
 
 //-------Functions----------
 
@@ -30,14 +37,67 @@ void modbus_task(void const * argument){
     }
 }
 
+int modbus_init(modbus_pcb_t* modbus_pcb){
+    int result = 0;
+
+    modbus_gpio_init();
+    modbus_pcb->state |= MODBUS_ST_READY;
+
+    return result;
+}
+
+int modbus_deinit(modbus_pcb_t* modbus_pcb){
+    int result = 0;
+
+    modbus_gpio_deinit();
+    modbus_pcb->state = MODBUS_ST_DISABLE;
+
+    return result;
+}
+
+int modbus_read_addr(void){
+    int result = 0;
+
+    if(HAL_GPIO_ReadPin(MDB_ADDR_7_PORT, MDB_ADDR_7_PIN)){
+        result |= 0x01;
+    }
+    if(HAL_GPIO_ReadPin(MDB_ADDR_6_PORT, MDB_ADDR_6_PIN)){
+        result |= 0x02;
+    }
+    if(HAL_GPIO_ReadPin(MDB_ADDR_5_PORT, MDB_ADDR_5_PIN)){
+        result |= 0x04;
+    }
+    if(HAL_GPIO_ReadPin(MDB_ADDR_4_PORT, MDB_ADDR_4_PIN)){
+        result |= 0x08;
+    }
+    if(HAL_GPIO_ReadPin(MDB_ADDR_3_PORT, MDB_ADDR_3_PIN)){
+        result |= 0x10;
+    }
+    if(HAL_GPIO_ReadPin(MDB_ADDR_2_PORT, MDB_ADDR_2_PIN)){
+        result |= 0x20;
+    }
+    if(HAL_GPIO_ReadPin(MDB_ADDR_1_PORT, MDB_ADDR_1_PIN)){
+        result |= 0x40;
+    }
+    if(HAL_GPIO_ReadPin(MDB_ADDR_0_PORT, MDB_ADDR_0_PIN)){
+        result |= 0x80;
+    }
+
+    return result;
+}
+
+int modbus_packet_recognise(u8* buf, u16 len){
+    int result = 0;
+
+    return result;
+}
 //-------Static functions----------
 
 /**
- * @brief Init switches GPIO of ModBUS address and rate
+ * @brief Init switches GPIO of ModBUS
  * @ingroup modbus
  *
  * Init MDB_ADDR switch pins
- * Init MDB_RATE switch pins
  */
 static void modbus_gpio_init(void){
     __HAL_RCC_GPIOA_CLK_ENABLE();
@@ -67,3 +127,21 @@ static void modbus_gpio_init(void){
     GPIO_InitStruct.Pin = MDB_ADDR_7_PIN;
     HAL_GPIO_Init (MDB_ADDR_7_PORT, &GPIO_InitStruct);
 }
+
+/**
+ * @brief Deinit switches GPIO of ModBUS
+ * @ingroup modbus
+ *
+ * Deinit MDB_ADDR switch pins
+ */
+static void modbus_gpio_deinit(void){
+    HAL_GPIO_DeInit(MDB_ADDR_0_PORT, MDB_ADDR_0_PIN);
+    HAL_GPIO_DeInit(MDB_ADDR_1_PORT, MDB_ADDR_1_PIN);
+    HAL_GPIO_DeInit(MDB_ADDR_2_PORT, MDB_ADDR_2_PIN);
+    HAL_GPIO_DeInit(MDB_ADDR_3_PORT, MDB_ADDR_3_PIN);
+    HAL_GPIO_DeInit(MDB_ADDR_4_PORT, MDB_ADDR_4_PIN);
+    HAL_GPIO_DeInit(MDB_ADDR_5_PORT, MDB_ADDR_5_PIN);
+    HAL_GPIO_DeInit(MDB_ADDR_6_PORT, MDB_ADDR_6_PIN);
+    HAL_GPIO_DeInit(MDB_ADDR_7_PORT, MDB_ADDR_7_PIN);
+}
+
