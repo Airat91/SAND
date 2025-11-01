157a2be4696dc8a584e76c5382e14de5c2566706
Air <Gitfanov.Ayrat@yandex.ru>
2025-11-02 00:26:13 +0500

    reg_get_by_name() and reg_get_by_ind() added

--- inc/reg.h
+++ inc/reg.h
@@ -8,6 +8,8 @@
 #ifndef __REG_H__
 #define __REG_H__
 /*add includes below */
+#include "sofi_reg.h"
+#include "string.h"
 #if(DEVICE_TYPE == DEFPCB)
     #include "rergs_defpcb.h"
 #elif(DEVICE_TYPE == SAND_AI)
@@ -33,6 +35,26 @@ extern "C" {
 //-------External variables------
 
 //-------Function prototypes----------
+
+//=======Regs search functions=======
+
+/**
+ * @brief Find register by name
+ * @param reg_name - pointer to name for find
+ * @ingroup reg
+ * @return  - pointer to reg,\n
+ *          NULL - register not found,\n
+ */
+sofi_prop_base_t* reg_get_by_name(char* reg_name);
+
+/**
+ * @brief Find register by index
+ * @param ind - index for find
+ * @ingroup reg
+ * @return  - pointer to reg,\n
+ *          NULL - register not found,\n
+ */
+sofi_prop_base_t* reg_get_by_ind(u16 ind);
 
 #ifdef __cplusplus
 }
--- src/reg.c
+++ src/reg.c
@@ -15,4 +15,34 @@
 
 //-------Functions----------
 
+sofi_prop_base_t* reg_get_by_name(char* reg_name){
+    sofi_prop_base_t* reg;
+    int reg_found = 0;
+
+    for(u16 i = 0; i < SOFI_PROP_BASE_REG_NUM; i++){
+        reg = (sofi_prop_base_t*)&sofi_prop_base_list[i];
+        if(strcmp(reg->name, reg_name) == 0){
+            reg_found = 1;
+            break;
+        }
+    }
+    if(reg_found == 0){
+        reg = NULL;
+    }
+
+    return reg;
+}
+
+sofi_prop_base_t* reg_get_by_ind(u16 ind){
+    sofi_prop_base_t* reg;
+
+    if(ind > SOFI_PROP_BASE_REG_NUM){
+        reg = NULL;
+    }else{
+        reg = (sofi_prop_base_t*)&sofi_prop_base_list[ind];
+    }
+
+    return reg;
+}
+
 //-------Static functions----------
